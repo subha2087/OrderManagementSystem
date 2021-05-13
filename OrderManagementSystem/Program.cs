cfe2d6274d25ed5d3aab66b09f0d8b1a70371b39
subha2087 <60758516+subha2087@users.noreply.github.com>
2021-05-13 01:42:37 +0100

    Refactoring

--- OrderManagementSystem/Program.cs
+++ OrderManagementSystem/Program.cs
@@ -7,14 +7,16 @@ namespace OrderManagementSystem
     {
         static void Main(string[] args)
         {
+            //Sample implementation for Payment
+            ProductPaymentModel model = new ProductPaymentModel();
             //To be replaced with dependancy Injection in startup 
             ProductPayment productPayment = new ProductPayment(new PackingService());
-            ProductPaymentModel model = new ProductPaymentModel();
             productPayment.MakePayment<ProductPaymentModel>(model);
 
+
+            BookPaymentModel bpmodel = new BookPaymentModel();
             //To be replaced with dependancy Injection in startup 
             BookPayment bookPayment = new BookPayment(new PackingService());
-            BookPaymentModel bpmodel = new BookPaymentModel();
             bookPayment.MakePayment<BookPaymentModel>(bpmodel);
 
         }
